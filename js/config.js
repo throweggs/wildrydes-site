d59644efa6629ecb4cce419ce12ea38581949c41
mr_cf <throweggs@me.com>
2025-08-29 10:09:35 +0100

    Set Cognito region to 'eu-west-2'

--- js/config.js
+++ js/config.js
@@ -2,10 +2,11 @@ window._config = {
     cognito: {
         userPoolId: 'eu-west-2_Pca80SnT6',
         userPoolClientId: '4d9b80m5i7ci9osl4gdcj6937k',
-        region: '' // e.g. us-east-2
+        region: 'eu-west-2'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
