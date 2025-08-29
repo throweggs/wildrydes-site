27c8c9f9475861caa71f61afbc60eb1692cdddfe
mr_cf <throweggs@me.com>
2025-08-29 10:19:41 +0100

    Update userPoolClientId in config.js

--- js/config.js
+++ js/config.js
@@ -1,12 +1,13 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-west-2_Pca80SnT6',
-        userPoolClientId: '4d9b80m5i7ci9osl4gdcj6937k',
+        userPoolClientId: '5p2nhdvqmi3d8bg0frdmb8e6as',
         region: 'eu-west-2'
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
